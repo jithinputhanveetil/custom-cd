22b672189e5e8b81358f6b14fe509c394dc96265
jithinputhanveetil <jithinappu.p6@gmail.com>
2019-08-14 17:28:27 +0530

    initial implementation

--- main.go
+++ main.go
@@ -2,17 +2,12 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"go-projects/customcd/customcd"
-	"runtime"
-	"time"
+
+	"github.com/jithinputhanveetil/custom-cd/customcd"
 )
 
 func main() {
-	t := time.Now()
 	flag.Parse()
 	prefix := flag.Arg(0)
 	customcd.SearchPath(prefix)
-	fmt.Println(runtime.NumCPU())
-	fmt.Println("time taken", time.Since(t))
 }
